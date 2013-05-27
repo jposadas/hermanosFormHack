b5d6adc9ee5ef49b9693274a7cdc1f6b153f0894
Juan Posadas <jposadas@stanford.edu>
2013-05-27 03:31:19 -0700

    added outer loop

--- fillGoogleForm.js
+++ fillGoogleForm.js
@@ -1,13 +1,17 @@
 var url = "https://docs.google.com/a/stanford.edu/forms/d/1Jn6odwjBrKJCEIdqnZxUyDDMDRYtjm5_2UThxfDSFmw/viewform";
+var times = 10;
 
-var win = window.open(url);
-var inputClass = "ss-q-short";
+for(var i = 0; i < times; i++) {
+	
+	var win = window.open(url);
+	var inputClass = "ss-q-short";
 
-var inputArray = win.document.getElementsByClassName(inputClass);
+	var inputArray = win.document.getElementsByClassName(inputClass);
+	var length = inputArray.length;
+	var inputString = "FUCKKKK KIKKKEEEE!!!"
 
-var length = inputArray.length;
-var inputString = "FUCKKKK KIKKKEEEE!!!"
-
-for(var i = 0; i < length; i++) { 
-	inputArray[i].value = inputString;
+	for(var j = 0; j < length; i++) { 
+		inputArray[j].value = inputString;
+	}
 }
+
